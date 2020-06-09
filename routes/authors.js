1c313bc3be36654e38b3c957ad1f6b8d3dca9b96
meteatbs <130202020@kocaeli.edu.tr>
2020-06-10 00:26:57 +0300

    initial

---
+++ routes/authors.js
@@ -0,0 +1,60 @@
+const express=require('express');
+const router=express.Router();
+const Author=require('../models/author');
+
+//All Authors Route
+
+router.get('/',async(req,res)=>{
+//post body'den gönderir, get kullandigimiz icin query kullanmalıyız
+    let searchOptions={}
+    if(req.query.name!=null && req.query.name!==''){
+        searchOptions.name=new RegExp(req.query.name,'i')
+    }
+    try {
+        const authors=await Author.find(searchOptions)
+        res.render('authors/index',{authors:authors,searchOptions:req.query})
+    } catch (err) {
+        res.redirect('/');
+    }
+  
+})
+
+//New Author Route
+
+router.get('/new',(req,res)=>{
+    res.render('authors/new',{author:new Author()})
+})
+
+
+//Create Author Route
+router.post('/',async(req,res)=>{
+    const author=new Author({
+        name:req.body.name
+    })
+    try {
+        
+        const newAuthor=await author.save();
+        // res.redirect(`authors/${newAuthor.id}`)
+        res.redirect(`authors`)
+
+    } catch (err) {
+                   res.render('authors/new',{
+                author:author,
+                errorMessage:'Error creating Author'
+    })
+}
+    // author.save((err,newAuthor)=>{
+    //     if (err) {
+    //         res.render('authors/new',{
+    //             author:author,
+    //             errorMessage:'Error creating Author'
+    //         })
+    //     }else{
+    //         // res.redirect(`authors/${newAuthor.id}`)
+    //         res.redirect(`authors`)
+    //     }
+    // })
+
+})
+
+module.exports=router
--- server.js
+++ server.js
@@ -7,8 +7,10 @@ const express=require('express');
 const app =express();
 
 const expressLayouts=require('express-ejs-layouts');
+const bodyParser=require('body-parser')
 
 const indexRouter=require('./routes/index')
+const authorRouter=require('./routes/authors')
 
 app.set('view engine','ejs')
 
@@ -18,15 +20,18 @@ app.set('layout', 'layouts/layout');
 
 app.use(expressLayouts);
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({limit:'10mb',extended:false}))
 
 const mongoose=require('mongoose');
 mongoose.connect(process.env.DATABASE_URL,{ useNewUrlParser:true})
 const db=mongoose.connection;
+
 
 db.on('error',error=>console.error(error))
 
 db.on('open',()=>console.log('DB Connected Succesfully'))
 
 app.use('/',indexRouter);
+app.use('/authors',authorRouter);
 
 app.listen(process.env.PORT || 3000);
